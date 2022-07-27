8063f7d8a9d45bdd80473ca2015e91d80ef33db7
Apoquinto <RCLlanesA@gmail.com>
2022-07-26 21:57:49 -0500

    feat: add apps gallery and change icons

--- project/app.js
+++ project/app.js
@@ -1,1 +1,32 @@
 import "./components/AppClock.js";
+import "./components/AppAppsGallery.js"
+
+const APPS = [
+    {
+        name: 'Notion',
+        icon: 'book',
+        link: 'https://www.notion.so/'
+    },
+    {
+        name: 'Github',
+        icon: 'github',
+        link: 'https://github.com/'
+    },
+    {
+        name: 'Codewars',
+        icon: 'code',
+        link: 'https://www.codewars.com/'
+    },
+    {
+        name: 'Figma',
+        icon: 'figma',
+        link: 'https://www.figma.com/'
+    },
+    {
+        name: 'Youtube',
+        icon: 'youtube',
+        link: 'https://www.figma.com/'
+    },
+];
+
+document.getElementById('apps-gallery').apps = APPS;
